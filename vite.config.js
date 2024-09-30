b1947641115816e33f7ad7302375f54e0b09f849
dinindukavinda <kavinda373@gmail.com>
2024-09-30 17:28:48 +0530

    main

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: "cleanig-co.test",
+        https: true,
+
+
+    },
+
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
